c16f89dc829ef5a8dc1a8a30dd3887b040763b48
Auf <azeezumarfaruk@gmail.com>
2023-06-07 15:03:28 +0100

    fix : updated logo

--- src/header.jsx
+++ src/header.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import logo from "./assets/gipperpay trans 1.png";
+import logo from "./assets/IMG-20230606-WA0107.jpg";
 import { RiMenu3Fill } from "react-icons/ri";
 
 const Header = () => {
@@ -26,7 +26,7 @@ const Header = () => {
         <>
             <div className=" header   w-full   header md:z-[100] md:top-0  z-[100] top-0  fixed">
                 <header className="flex justify-between w-full pt-[40px] pb-[20px] backdrop-blur-[4px]   items-center px-[4%] sm:px-[40px] lg:px-[100px]">
-                    <img src={logo} alt="" className="w-[36px] h-[36px] " />
+                    <img src={logo} alt="" className="w-[40px] h-[40px] " />
                     <RiMenu3Fill className="w-8 h-8 fill-white  fill-[#FF007A] " />
                 </header>
             </div>
